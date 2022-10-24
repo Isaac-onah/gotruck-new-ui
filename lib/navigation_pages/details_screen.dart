dbfcfc74bf48adedbbcfa27832de609e4235c813
Aiyzihk <45091852+Isaac-onah@users.noreply.github.com>
2022-10-24 14:46:03 +0100

    update

--- lib/navigation_pages/details_screen.dart
+++ lib/navigation_pages/details_screen.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import '../constants/constants.dart';
+import '../models/cart_controller.dart';
 import '../models/utils.dart';
 import 'components/shimmer.dart';
 
@@ -62,21 +67,22 @@ class allCarDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cartController = Get.put(CartController());
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        foregroundColor:Color(0xFF0FA958),
+        foregroundColor:const Color(0xFF0FA958),
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 0.0),
+        padding: const EdgeInsets.symmetric(horizontal: 0.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment:CrossAxisAlignment.start,
           children: [
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 15.0),
+              padding:  const EdgeInsets.symmetric(horizontal: 15.0),
               child: Column(
                 children: [
                   Row(
@@ -89,14 +95,14 @@ class allCarDetail extends StatelessWidget {
                           crossAxisAlignment:CrossAxisAlignment.start,
                           children: [
                             Padding(
-                              padding:EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
-                              child: Text('${model == 'null' ? 'n/a' : model}', style: MainHeading,textAlign: TextAlign.left,),
+                              padding:const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+                              child: Text(model == 'null' ? 'n/a' : model, style: MainHeading,textAlign: TextAlign.left,),
                             ),
                             Padding(
                               padding: const EdgeInsets.symmetric(horizontal: 8.0,),
                               child: Text(
-                                '${brand == 'null' ? 'n/a' : brand}',
-                                style:TextStyle(fontSize: 20, ),textAlign: TextAlign.left,
+                                brand == 'null' ? 'n/a' : brand,
+                                style:const TextStyle(fontSize: 20, ),textAlign: TextAlign.left,
                               ),
                             ),
                           ],
@@ -104,18 +110,49 @@ class allCarDetail extends StatelessWidget {
                       ),
                       SpecificsAmount(
                         name: 'Amount',
-                        name2: '${price == 'null' ? 'n/a' : formatCurrency.format(
+                        name2: price == 'null' ? 'n/a' : formatCurrency.format(
                             double.parse(
                                 (price)
-                                    .toString()))}',
+                                    .toString())),
                       )
                     ],
                   ),
                   Center(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
-                      child: Hero(tag: '${model == 'null' ? 'n/a' : model}', child: Image.network('${imageurl == 'null' ? 'n/a' : imageurl}', width: 300,)),
+                      child: Hero(tag: model == 'null' ? 'n/a' : model, child: Image.network(imageurl == 'null' ? 'n/a' : imageurl, width: 300,)),
                     ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment:CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment:CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding:const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+                              child: Text(model == 'null' ? 'n/a' : model, style: MainHeading,textAlign: TextAlign.left,),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0,),
+                              child: Text(
+                                brand == 'null' ? 'n/a' : brand,
+                                style:const TextStyle(fontSize: 20, ),textAlign: TextAlign.left,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                          padding: EdgeInsets.all(0),
+                          onPressed: (){
+                            cartController.addProduct(usar);
+                          },
+                          icon: const Icon(CommunityMaterialIcons.cart_plus, color:kPrimaryColor,size: 20,))
+                    ],
                   ),
                 ],
               ),
@@ -123,7 +160,7 @@ class allCarDetail extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: Column(
 
                     mainAxisAlignment: MainAxisAlignment.start,
@@ -134,65 +171,65 @@ class allCarDetail extends StatelessWidget {
                         children: [
                           SpecificsCard(
                             name: 'Engine Type',
-                            name2: '${enginetype == 'null' ? 'n/a' : enginetype}',
+                            name2: enginetype == 'null' ? 'n/a' : enginetype,
                             imageurl: 'engine.svg',
                           ),
 
-                          SizedBox(width: 5),
+                          const SizedBox(width: 5),
                           SpecificsCard(
                             name: 'Fuel Tank',
-                            name2: '${fueltank == 'null' ? 'n/a' : '${fueltank} Ltrs'}',
+                            name2: fueltank == 'null' ? 'n/a' : '${fueltank} Ltrs',
                             imageurl: 'gas.svg',
                           ),
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
                           SpecificsCard(
                             name: 'Number of Tires',
-                            name2: '${tire == 'null' ? 'n/a' : tire}',
+                            name2: tire == 'null' ? 'n/a' : tire,
                             imageurl: 'tire.svg',
                           ),
-                          SizedBox(width: 5),
+                          const SizedBox(width: 5),
                           SpecificsCard(
                             name: 'Mileage',
-                            name2: '${milage == 'null' ? 'n/a' : '${milage}km'}',
+                            name2: milage == 'null' ? 'n/a' : '${milage}km',
                             imageurl: 'digital-display-svgrepo-com.svg',
                           )
                         ],
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
                           SpecificsCard(
                             name: 'Year',
-                            name2: '${year == 'null' ? 'n/a' : year}',
+                            name2: year == 'null' ? 'n/a' : year,
                             imageurl: 'calendar.svg',
                           ),
-                          SizedBox(width: 5),
+                          const SizedBox(width: 5),
                           SpecificsCard(
                             name: 'Speed',
-                            name2: '${maxspeed == 'null' ? 'n/a' : '${maxspeed}km/h'}',
+                            name2: maxspeed == 'null' ? 'n/a' : '${maxspeed}km/h',
                             imageurl: 'speed.svg',
                           )
                         ],
                       ),
-                      SizedBox(height: 20),
+                      const SizedBox(height: 20),
                       Padding(
-                        padding: EdgeInsets.all(8),
+                        padding: const EdgeInsets.all(8),
                         child: Text(
                           'Specifications',
                           style: GoogleFonts.poppins(
-                              textStyle: TextStyle(
+                              textStyle: const TextStyle(
                                   color: Colors.black87,
                                   fontSize: 15,
                                   fontWeight: FontWeight.bold)),
                         ),
                       ),
-                      SizedBox(height: 10),
+                      const SizedBox(height: 10),
                       Column(
                         children: [
                           Row(
@@ -200,59 +237,59 @@ class allCarDetail extends StatelessWidget {
                             children: [
                               SpecificsCard(
                                 name: 'Axle',
-                                name2: '${rearaxle == 'null' ? 'n/a' : rearaxle}',
+                                name2: rearaxle == 'null' ? 'n/a' : rearaxle,
                                 imageurl: 'truck.svg',
                               ),
                             ],
                           ),
-                          SizedBox(height: 5),
+                          const SizedBox(height: 5),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
                               SpecificsCard(
                                 name: 'Max Torque',
-                                name2: '${maxtorque == 'null' ? 'n/a' : maxtorque}',
+                                name2: maxtorque == 'null' ? 'n/a' : maxtorque,
                                 imageurl: 'wrench.svg',
                               ),
                             ],
                           ),
-                          SizedBox(height: 5),
+                          const SizedBox(height: 5),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
                               SpecificsCard(
                                 name: 'Tonnage Capacity',
-                                name2: '${tonnage == 'null' ? 'n/a' : tonnage}',
+                                name2: tonnage == 'null' ? 'n/a' : tonnage,
                                 imageurl: 'weti.svg',
                               ),
                             ],
                           ),
-                          SizedBox(height: 5),
+                          const SizedBox(height: 5),
                           Row(
                             children: [
                               SpecificsCard(
                                 name: 'Transmission',
-                                name2: '${transmissionmodel == 'null' ? 'n/a' : transmissionmodel}',
+                                name2: transmissionmodel == 'null' ? 'n/a' : transmissionmodel,
                                 imageurl: 'manual.svg',
                               ),
                             ],
                           ),
-                          SizedBox(height: 5),
+                          const SizedBox(height: 5),
                           Row(
                             children: [
                               SpecificsCard(
                                 name: 'Driving Type',
-                                name2: '${drivingtype == 'null' ? 'n/a' : drivingtype}',
+                                name2: drivingtype == 'null' ? 'n/a' : drivingtype,
                                 imageurl: 'steering.svg',
                               ),
                             ],
                           ),
-                          SizedBox(height: 5),
+                          const SizedBox(height: 5),
                           Row(
                             children: [
                               SpecificsCard(
                                 name: 'Clutch Type',
-                                name2: '${clutch == 'null' ? 'n/a' : clutch}',
+                                name2: clutch == 'null' ? 'n/a' : clutch,
                                 imageurl: 'tyy.svg',
                               ),
                             ],
